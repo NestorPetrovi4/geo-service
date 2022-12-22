d339badba1e556568858cb8dacc03495bfa4dab8
Oleg Nesterov <oleg9988@rambler.ru>
2022-12-22 15:51:18 +0300

    Исправлены тесты на использование библиотеки Mockito

--- src/test/java/TestMessageSender.java
+++ src/test/java/TestMessageSender.java
@@ -1,14 +1,12 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
-import ru.netology.geo.GeoServiceImplMock;
 import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImplMock;
 import ru.netology.sender.MessageSender;
 import ru.netology.sender.MessageSenderImpl;
 
@@ -16,43 +14,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestMessageSender {
-    GeoService geoServiceImplMock;
-    LocalizationService localizationServiceImplMock;
+    GeoService geoService;
+    LocalizationService localizationService;
     Map<String, String> headers;
 
     @BeforeEach
     public void initTest() {
-        geoServiceImplMock = new GeoServiceImplMock();
-        localizationServiceImplMock = new LocalizationServiceImplMock();
+        geoService = Mockito.mock(GeoService.class);
+        localizationService = Mockito.mock(LocalizationService.class);
         headers = new HashMap<>();
     }
 
     @Test
-    public void senderServiceTestRussia() {
-        MessageSenderImpl messageSender = new MessageSenderImpl(geoServiceImplMock, localizationServiceImplMock);
+    public void senderServiceTestRussiaIp() {
+        Mockito.when(geoService.byIp(Mockito.startsWith("172."))).thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
+        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
-        String result = "Добро пожаловать";
-        Assertions.assertEquals(messageSender.send(headers), result);
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
+        Assertions.assertEquals(messageSender.send(headers), "Добро пожаловать");
     }
 
     @Test
-    public void senderServiceTestUSA() {
+    public void senderServiceTestUsaIp() {
+        Mockito.when(geoService.byIp(Mockito.startsWith("96."))).thenReturn(new Location("New York", Country.USA, null, 0));
+        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.123.12.19");
-        MessageSender messageSender = Mockito.mock(MessageSender.class);
-        Mockito.when(messageSender.send(headers)).thenReturn("USA");
-        Assertions.assertEquals(messageSender.send(headers), "USA");
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
+        Assertions.assertEquals(messageSender.send(headers), "Welcome");
     }
 
     @Test
-    public void checkByIpTest() {
-        Location resultLocation = new Location("Moscow", Country.RUSSIA, null, 0);
-        Assertions.assertEquals(resultLocation.getCity(), geoServiceImplMock.byIp("172.123.12.19").getCity());
+    public void geoServiceByIpPositiveRussiaTest() {
+        Mockito.when(geoService.byIp(Mockito.startsWith("172."))).thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
+        Assertions.assertEquals(Country.RUSSIA, geoService.byIp("172.123.12.19").getCountry());
     }
 
     @Test
-    public void checkLocaleTest() {
-        final String result = "Добро пожаловать";
-        Assertions.assertEquals(result, localizationServiceImplMock.locale(Country.RUSSIA));
+    public void localizationServiceLocalPositiveRussiaUsaTest() {
+        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
+        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+        Assertions.assertTrue("Welcome".equals(localizationService.locale(Country.USA)) || "Добро пожаловать".equals(localizationService.locale(Country.RUSSIA)));
     }
 
 }
